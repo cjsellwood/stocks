8a64b9cc1069eeff52b6c05e44327a4a24191826
cjsellwood <c.sellwood22@gmail.com>
2021-04-29 10:54:46 +1000

    Increase jwt expiration

--- utils/issueJWT.js
+++ utils/issueJWT.js
@@ -2,8 +2,8 @@ const jsonwebtoken = require("jsonwebtoken")
 
 module.exports = (user) => {
   const _id = user._id;
-  // Expires in 1 day
-  const expiresIn = `${1000 * 60 * 60 * 24}`;
+  // Expires in 7 days
+  const expiresIn = `${1000 * 60 * 60 * 24 * 7}`;
   const payload = {
     sub: _id,
     iat: Date.now(),
